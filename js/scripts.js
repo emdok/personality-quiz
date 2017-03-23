f00d0a412f1f729cac16ed359b30b16a42a0360f
Benjamin deRochemont and Emily Dokken <emily@>
2017-03-22 20:22:32 -0700

    created backend function to give advice based on stress level

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,46 @@
 //Back End Logic
+var evaluation = function(stressEffects, stressSymptoms, copingMethods) {
+  var result = "";
+  if ((stressEffects.length + stressSymptoms.length > 5) && (copingMethods.length < 1)) {
+    result = "You seem really stressed. Here is a list of recommended stress management methods:";
+  } else if ((stressEffects.length + stressSymptoms.length > 5) && (copingMethods.length < 3)) {
+    result = "You seem to be utilizing some of the stress reduction methods, but maybe you should incorporate a few more. Here is a list of some reccommended stress reduction methods:";
+  } else if ((stressEffects.length + stressSymptoms.length > 5) && (copingMethods.length > 4)) {
+    result = "You are doing a great job at trying to reduce your stress. Keep at it!";
+  } else {
+    result = "You don't even need to be here, you're good to go."
+  }
 
+  return result;
+}
 
 
 //Front End Logic
 
 $(function(){
+
+  var stressEffects = [];
+  var stressSymptoms = [];
+  var copingMethods = [];
+
   $('#stress-effects').submit(function(event){
     event.preventDefault();
-    var stressEffects = [];
+
     $('input:checkbox[name=stress-effects]:checked').each(function(){
       var effect = $(this).val();
       stressEffects.push(effect);
     });
-    var stressSymptoms = [];
+
     $('input:checkbox[name=stress-symptoms]:checked').each(function(){
       var symptom = $(this).val();
       stressSymptoms.push(symptom);
     });
-    var copingMethods = [];
+
     $('input:checkbox[name=coping-methods]:checked').each(function(){
       var method = $(this).val();
       copingMethods.push(method);
     });
-    console.log(stressEffects);
-    console.log(stressSymptoms);
-    console.log(copingMethods);
+
+    $(".text").text(evaluation(stressEffects, stressSymptoms, copingMethods));
   });
 });
